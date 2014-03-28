eabdc51ff491cfb194b516794253194743ec2eeb
titanium <titanium@dev2.rasoiclub.com>
2014-03-28 19:14:50 +0530

    first rainbow commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainbow::Application.config.secret_key_base = 'ac52cda5d2164aa55cfa15ca8e17365c7642959191adda555f21bbb8d8d64f00519d5e4c3e1f4c15f51568db183e8df4fa8160874ad579c740c48b2d895053ef'
